02bb38bf4034289eacbfd8452026344d3f2d0cb8
Patryk Bernasiewicz <kontakt@patrykb.pl>
2017-09-27 21:51:09 +0200

    added duplicate check for vets

--- server/policies/vets.policy.js
+++ server/policies/vets.policy.js
@@ -1,5 +1,7 @@
+const { Vet } = require('../models');
+
 module.exports = {
-  suggest (req, res, next) {
+  async suggest (req, res, next) {
     // Check if required fields are present
     const requiredFields = ['title', 'city', 'address'];
     requiredFields.forEach((item) => {
@@ -12,6 +14,12 @@ module.exports = {
       if (!/^[0-9+ -\(\)]{7,15}$/.test(req.body['phone'])) {
         res.status(400).json({ message: 'Numer telefonu jest nieprawidłowy.' });
       }
+    }
+
+    // Check for duplicate
+    const vets = await Vet.findOne({ where: { title: req.body.title, city: req.body.city } });
+    if (vets) {
+      res.status(400).json({ message: `${req.body.title} już istnieje w mieście ${req.body.city} lub ktoś już zasugerował jego dodanie.` });
     }
 
     next();
